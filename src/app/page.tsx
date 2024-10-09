7a5e61f260cd00fdedb2cd9e73c2754d583068b2
Steve Swanson <steve.s@whogivesacrap.org>
2024-10-09 10:05:42 -0500

    style: added a new favicon
    chore: changed the name of the app to Pennies in the title
    
    chore: code cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,6 +54,23 @@ export default function Home() {
 			</main>
 			<footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
 				<a
+					href="https://iconscout.com/icons/savings"
+					className="text-underline font-size-sm"
+					target="_blank"
+					rel="noreferrer"
+				>
+					savings
+				</a>{' '}
+				by{' '}
+				<a
+					href="https://iconscout.com/contributors/iconscout"
+					className="text-underline font-size-sm"
+					target="_blank"
+					rel="noreferrer"
+				>
+					IconScout Store
+				</a>
+				<a
 					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
 					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
 					target="_blank"
