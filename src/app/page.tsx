c83c70c05e26af6c84c19e7dd6ac971fdd536460
Steve Swanson <steve.s@whogivesacrap.org>
2024-10-09 12:18:00 -0500

    feat: added daisyui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,11 @@ export default function Home() {
 
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-			{isLoggedIn !== 'true' && <Link href="signup">Signup!</Link>}
+			{isLoggedIn !== 'true' && (
+				<Link href="signup" className="btn btn-primary">
+					Signup!
+				</Link>
+			)}
 
 			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 				<Image
