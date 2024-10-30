b865b94a3da1a08b52cfefc444947ef6db85a508
Steve Swanson <steve.s@whogivesacrap.org>
2024-10-30 13:01:18 -0500

    feat: setup everything possible for adding a category except the api

---
+++ src/app/categories/components/Categories/CustomCategories.tsx
@@ -0,0 +1,85 @@
+'use client';
+
+import apiCall from '@/app/accounts/scripts/apiCall';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+interface CustomCategoriesProps {
+	categories: string[];
+	uid: string | false;
+}
+
+export interface CategoryFormData {
+	category: string;
+}
+
+export default function CustomCategories({
+	categories,
+	uid,
+}: CustomCategoriesProps) {
+	const [error, setError] = useState('');
+	const [success, setSuccess] = useState('');
+	const {
+		formState: { errors },
+		handleSubmit,
+		register,
+		reset,
+	} = useForm<CategoryFormData>();
+
+	function handleCreateCategory(formData: CategoryFormData) {
+		console.error(errors);
+		setError('');
+		setSuccess('');
+		if (!Object.keys(errors).length && uid) {
+			const { category } = formData;
+
+			apiCall('/api/category-create', {
+				onError: (msg) => setError(msg),
+				onSuccess: (msg) => setSuccess(msg),
+				payload: {
+					category,
+					uid,
+				},
+				reload: '/categories',
+			});
+
+			setSuccess('Success');
+			reset();
+		}
+	}
+
+	return (
+		<div>
+			{error.length > 0 && (
+				<div className="alert alert-error mb-6 text-white font-bold">
+					{error}
+				</div>
+			)}
+			{success.length > 0 && (
+				<div className="alert alert-success mb-6 text-white font-bold">
+					{success}
+				</div>
+			)}
+			<form
+				className="form-control join join-horizontal"
+				onSubmit={handleSubmit(handleCreateCategory)}
+			>
+				<input
+					type="text"
+					className={`input input-text input-sm input-bordered join-item${errors?.category ? ' input-error' : ''}`}
+					{...register('category', { required: true })}
+				/>
+				<button
+					type="submit"
+					className={'btn btn-success btn-sm text-white font-bold join-item'}
+				>
+					Create
+				</button>
+			</form>
+
+			{categories.map((category) => (
+				<div key={category}>{category}</div>
+			))}
+		</div>
+	);
+}
---
+++ src/app/categories/components/Categories/index.tsx
@@ -0,0 +1,40 @@
+import useServerCookie from '@/app/hooks/useServerCookie';
+import type { UserData } from '@/app/types';
+import { defaultCategories, USER } from '@/app/constants';
+import CustomCategories from './CustomCategories';
+
+export default function Categories() {
+	const [userCookieData] = useServerCookie<UserData>(USER);
+	const uid =
+		typeof userCookieData === 'object'
+			? (userCookieData as UserData).uid
+			: false;
+	const categories =
+		typeof userCookieData === 'object' && userCookieData.categories
+			? (userCookieData.categories as string[])
+			: [];
+
+	return (
+		<div>
+			<div className="flex">
+				<div className="w-3/12 pr-4">
+					<h3>Default Categories</h3>
+
+					<ul className="list-disc">
+						{defaultCategories.map((category) => (
+							<li className="py-0 my-0" key={category}>
+								{category}
+							</li>
+						))}
+					</ul>
+				</div>
+
+				<div className="w-9/12">
+					<h3>Custom Categories</h3>
+
+					<CustomCategories categories={categories} uid={uid} />
+				</div>
+			</div>
+		</div>
+	);
+}
