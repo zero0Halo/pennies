db103ab0be1ba0f09dfac9ce3b88c838ddfd34d1
Steve Swanson <steve.s@whogivesacrap.org>
2024-12-01 16:37:29 -0600

    feat: first commit
    feat: created HeroStep component

---
+++ src/app/components/home/HeroStep.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import type { ReactElement } from 'react';
+
+interface HeroStepArguments {
+	link: string;
+	linkText: string;
+	subTitle?: string;
+	title: string;
+}
+
+export default function HeroStep({
+	link,
+	linkText,
+	subTitle,
+	title,
+}: HeroStepArguments): ReactElement {
+	return (
+		<div className="hero bg-accent">
+			<div className="hero-content text-center pb-8">
+				<div className="max-w-md">
+					<h2>{title}</h2>
+					<h3>{subTitle}</h3>
+
+					<Link href={link} className="btn btn-primary text-xl">
+						{linkText}
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+}
