f85b85b586f00af77aae426337987fb979812034
Steve Swanson <steve.s@whogivesacrap.org>
2024-12-01 16:41:01 -0600

    chore: used HeroStep for each step in the new user flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import NextCruft from './components/NextCruft';
 import { useIsLoggedIn, useServerCookie } from '@/app/hooks/server';
 import type { AccountData, UserData } from './types';
@@ -26,45 +25,27 @@ export default function Home() {
 			)}
 
 			{isLoggedIn && noAccounts && (
-				<div className="hero bg-accent">
-					<div className="hero-content text-center pb-8">
-						<div className="max-w-md">
-							<h2>You Need An Account To Start Saving Pennies!</h2>
-
-							<Link href="accounts" className="btn btn-primary">
-								Go To Accounts
-							</Link>
-						</div>
-					</div>
-				</div>
+				<HeroStep
+					link="accounts"
+					linkText="Go To Accounts"
+					title="You Need An Account To Start Saving Pennies!"
+				/>
 			)}
 
 			{isLoggedIn && !noAccounts && noCategories && (
-				<div className="hero bg-accent">
-					<div className="hero-content text-center pb-8">
-						<div className="max-w-md">
-							<h2>Create Your Categories!</h2>
-
-							<Link href="categories" className="btn btn-primary">
-								Go To Categories
-							</Link>
-						</div>
-					</div>
-				</div>
+				<HeroStep
+					link="categories"
+					linkText="Go To Categories"
+					title="Create Your Categories!"
+				/>
 			)}
 
 			{isLoggedIn && !noAccounts && !noCategories && (
-				<div className="hero bg-accent">
-					<div className="hero-content text-center pb-8">
-						<div className="max-w-md">
-							<h2>Import Your Bank Data!</h2>
-
-							<Link href="csv-upload" className="btn btn-primary">
-								Go To CSV Upload
-							</Link>
-						</div>
-					</div>
-				</div>
+				<HeroStep
+					link="csv-upload"
+					linkText="Go To CSV Upload"
+					title="Import Your Bank Data!"
+				/>
 			)}
 
 			{false && <NextCruft />}
