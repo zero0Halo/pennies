2d63711b3be9b8c89d78dfd4ec148e0c47b2c28a
Steve Swanson <steve.s@whogivesacrap.org>
2025-01-28 17:23:41 -0600

    feat: group/select now accepts hashes or uid

--- src/app/api/group/select/route.ts
+++ src/app/api/group/select/route.ts
@@ -4,15 +4,17 @@ import type { GroupData } from '@/app/types';
 import { GROUPS } from '@/app/constants';
 
 export async function POST(req: Request) {
-	const { uid } = await req.json();
+	// This endpoint allows group(s) to be selected by EITHER hashes or uid, not both
+	const { hashes, uid } = await req.json();
 
 	const superiorBase = await superiorBaseFactory();
-	const { error, success: response } = await superiorBase
-		.from(GROUPS)
-		.select('*')
-		.eq('uid', uid)
-		.single()
-		.go<GroupData>();
+	let query = superiorBase.from(GROUPS).select('*');
+	query = hashes ? query.in('hash', hashes) : query.eq('uid', uid).single();
+
+	const { error, success: response } = await query.go<
+		GroupData | GroupData[]
+	>();
+
 	if (error || response === null) return error;
 
 	return response;
